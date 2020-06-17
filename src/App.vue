d1fa67d62444ff5cca47f39064003e6cdd9b5e72
Akmatali <sirazhidinovakmatali@gmail.com>
2020-06-17 17:45:33 +0600

    I added two component todo, todos, deleted HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todos from './components/Todos';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Go workout',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Do laundry',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Cook food',
+          completed: false
+        },
+        {
+          id: 4,
+          title: 'Clean up room',
+          completed: false
+        },
+        {
+          i: 5,
+          title: 'Finish work',
+          completed: false
+        }
+      ],
+    }
+  },
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>My todolist</h2>
+  </div>
+</template>
+<script>
+import Todo from './Todo';
+
+export default {
+  name: 'Todos',
+  components: {
+    Todo
+  }
+}
+</script>
+<style scoped>
+
+</style>
