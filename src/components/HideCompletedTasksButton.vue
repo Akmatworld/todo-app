ff482ca382c04b271e8ad478fd96e3469b0e1fec
Akmatali <sirazhidinovakmatali@gmail.com>
2020-06-26 17:43:18 +0600

    Added components for hide btn and completed tasks

---
+++ src/components/HideCompletedTasksButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <div >
+    <div class="hide-completed-bnt" @click="$emit('hide-completed-tasks')">Hide Completed Tasks</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HideCompletedTasksButton',
+  data () {
+    return {
+      show: 'Show Completed Tasks',
+      hide: 'Hide Completed Tasks'
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+  .hide-completed-list {
+    display: flex;
+    justify-content: center;
+  }
+  .hide-completed-bnt {
+    display: inline-block;
+    background-color: #45c7c1;
+    color: #fff;
+    border-radius: 5px;
+    padding: 0 10px 0;
+    &:hover {
+      cursor: pointer;
+    }
+  }
+</style>
---
+++ src/components/completedTodo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div :class="{ 'completed': isCompleted }">
+      <div class="item-div">
+        <div class="completed-container">
+          <input type="checkbox" class="completed-checkbox" v-model="isCompleted">
+        </div>
+        <p class="item-p" :class="{completed: isCompleted}">{{ ctodo.title }}</p>
+        <div class="trash-icon" @click="$emit('delete-todo')">
+          <i class="far fa-trash-alt"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'completedTodo',
+  props: ['ctodo'],
+  data () {
+    return {
+      isCompleted: this.ctodo.completed
+    }
+  },
+  watch: {
+    isCompleted (val, oldVal) {
+      this.$emit('move-back')
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+</style>
